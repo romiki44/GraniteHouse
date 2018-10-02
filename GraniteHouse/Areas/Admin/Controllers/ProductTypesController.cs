528c3c15c27210eba6df105b4658dc16838f050b
Roman Kiss <romiki44@hotmail.com>
2018-10-02 20:16:58 +0200

    Add project files.

---
+++ GraniteHouse/Areas/Admin/Controllers/ProductTypesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraniteHouse.Data;
+using GraniteHouse.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GraniteHouse.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductTypesController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ProductTypesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_db.ProductTypes.ToList());
+        }
+
+        //Get Create 
+        public IActionResult Create()
+        {
+            Console.Write("Create Get");
+            return View();
+        }
+
+        //POST Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ProductTypes productTypes)
+        {
+            Console.Write("Create Post");
+            if(ModelState.IsValid)
+            {
+                _db.Add(productTypes);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productTypes); 
+        }
+
+        //Get Edit 
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if(id==null)
+            {
+                return NotFound();
+            }
+            var productType = await _db.ProductTypes.FindAsync(id);
+            if(productType==null)
+            {
+                return NotFound();
+            }
+
+            return View(productType);
+        }
+
+        //POST Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, ProductTypes productTypes)
+        {
+            if(id!=productTypes.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Update(productTypes);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productTypes);
+        }
+
+        //Get Details
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var productType = await _db.ProductTypes.FindAsync(id);
+            if (productType == null)
+            {
+                return NotFound();
+            }
+
+            return View(productType);
+        }
+
+        //Get Delete
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var productType = await _db.ProductTypes.FindAsync(id);
+            if (productType == null)
+            {
+                return NotFound();
+            }
+
+            return View(productType);
+        }
+
+        //POST Delete
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var productTypes = await _db.ProductTypes.FindAsync(id);
+            _db.ProductTypes.Remove(productTypes);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+            
+        }
+    }
+}
